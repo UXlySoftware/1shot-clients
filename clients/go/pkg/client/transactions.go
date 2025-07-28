63341f7808e4884c222f36e03eeb13475ea6d30c
Charlie Sibbach <charlie@uxly.software>
2025-07-28 14:26:45 -0700

    Update transactions.go with latest function signatures.

--- clients/go/pkg/client/transactions.go
+++ clients/go/pkg/client/transactions.go
@@ -7,27 +7,29 @@ import (
 	"github.com/antihax/optional"
 )
 
-// Transactions handles all transaction execution-related operations
+// Transactions handles all transaction-related operations
 type Transactions struct {
 	api        *swagger.TransactionsApiService
 	businessId string
 }
 
-// Get gets a specific transaction execution by its ID
-func (e *Transactions) Get(ctx context.Context, executionId string) (*swagger.Transaction, error) {
-	resp, _, err := e.api.TransactionsContractMethodIdGet(ctx, executionId)
+// Get gets a specific transaction by its ID
+func (e *Transactions) Get(ctx context.Context, transactionId string) (*swagger.Transaction, error) {
+	resp, _, err := e.api.TransactionsContractMethodIdGet(ctx, transactionId)
 	if err != nil {
 		return nil, err
 	}
 	return &resp, nil
 }
 
-// List lists transaction executions for a business with optional filters
+// List lists transactions for a business with optional filters
 func (e *Transactions) List(
 	ctx context.Context,
 	pageSize, page *int32,
 	chainId *swagger.EChain,
-	walletId, contractMethodId, apiCredentialId, userId *string,
+	status *string,
+	walletId, contractMethodId, apiCredentialId, userId, memo *string,
+	createdAfter, createdBefore *int64,
 ) ([]swagger.Transaction, error) {
 	opts := &swagger.TransactionsApiBusinessBusinessIdTransactionsGetOpts{}
 	if pageSize != nil {
@@ -38,6 +40,9 @@ func (e *Transactions) List(
 	}
 	if chainId != nil {
 		opts.ChainId = optional.NewInterface(*chainId)
+	}
+	if status != nil {
+		opts.Status = optional.NewString(*status)
 	}
 	if walletId != nil {
 		opts.WalletId = optional.NewInterface(*walletId)
@@ -50,6 +55,15 @@ func (e *Transactions) List(
 	}
 	if userId != nil {
 		opts.UserId = optional.NewInterface(*userId)
+	}
+	if memo != nil {
+		opts.Memo = optional.NewString(*memo)
+	}
+	if createdAfter != nil {
+		opts.CreatedAfter = optional.NewFloat64(float64(*createdAfter))
+	}
+	if createdBefore != nil {
+		opts.CreatedBefore = optional.NewFloat64(float64(*createdBefore))
 	}
 
 	resp, _, err := e.api.BusinessBusinessIdTransactionsGet(ctx, e.businessId, opts)
